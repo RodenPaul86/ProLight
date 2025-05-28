b0d3cbb9607e6f22755d133a4d85023fe0db6476
Paul Roden Jr <per312@icloud.com>
2025-05-28 12:38:44 -0500

    Initial Commit

---
+++ ProLight/ProLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProLightApp.swift
+//  ProLight
+//
+//  Created by Paul  on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ProLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
