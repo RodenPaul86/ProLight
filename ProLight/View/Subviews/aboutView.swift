84d75ac80b32074a1a01b33f7a00d71bfa92d31c
Paul Roden Jr <per312@icloud.com>
2026-03-01 21:38:23 -0600

    Fixed the about view to show accurate info.

--- ProLight/View/Subviews/aboutView.swift
+++ ProLight/View/Subviews/aboutView.swift
@@ -21,14 +21,13 @@ struct aboutView: View {
                         customRow(icon: "hammer", firstLabel: "Build", secondLabel: Bundle.main.appBuild)
                     }
                     
-                    Section(footer: Text("© 2025 Paul Roden II. All Rights Reserved.")) {
+                    Section(footer: Text("© 2016 - \(Date(), format: .dateTime.year()) Paul Roden Jr. All Rights Reserved, Made in USA 🇺🇸.")) {
                         customRow(icon: "laptopcomputer", firstLabel: "Developer", secondLabel: "Paul Roden Jr.")
                         
-                        Text("DocMatic was crafted by a single dedicated indie developer, who relies on your support to grow. \n\nTogether, we'll continuously expand and enrich the experience, ensuring you always get the most out of your subscription. \n\nThank you for being a part of this journey!")
+                        Text("ProLight was crafted by a single dedicated indie iOS developer, who relies on your support to grow. \n\nTogether, we'll continuously expand and enrich the experience, ensuring you always get the most out of your subscription. \n\nThank you for being a part of this journey!")
                             .font(.subheadline)
                         
-                        customRow(icon: "link", firstLabel: "My Website", secondLabel: "", url: "https://paulrodenjr.org")
-                        customRow(icon: "link", firstLabel: "DocMatic Website", secondLabel: "", url: "https://docmatic.app")
+                        customRow(icon: "link", firstLabel: "My Website", secondLabel: "", url: "https://paulrodenjr.dev")
                         customRow(icon: "link", firstLabel: "GitHub", secondLabel: "", url: "https://github.com/RodenPaul86")
                     }
                 }
