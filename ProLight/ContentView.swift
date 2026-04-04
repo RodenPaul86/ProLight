ced43db9d46d87c292032e24c6ff2fbab39c21e4
Paul Roden Jr <per312@icloud.com>
2026-04-04 18:42:51 -0500

    Made some fixes

--- ProLight/ContentView.swift
+++ ProLight/ContentView.swift
@@ -40,4 +40,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
--- ProLight/View/Subviews/feedbackView.swift
+++ ProLight/View/Subviews/feedbackView.swift
@@ -141,7 +141,6 @@ struct feedbackView: View {
                 }
             }
             .hideFloatingTabBar(hideTabBar)
-            
         }
     }
     
