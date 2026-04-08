80a9295a4e960c3f64c4f5c2ce159c7632cef5cc
Paul Roden Jr <per312@icloud.com>
2026-04-08 00:29:29 -0500

    Fixing new map view

--- ProLight/ContentView.swift
+++ ProLight/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
         FloatingTabView(selection: $activeTab) { tab, tabBarHeight in
             switch tab {
             case .home: HomeView(tabBarHeight: tabBarHeight)
-            case .map: NightWalkMapView(tabBarHeight: tabBarHeight)
+            case .map: mapView()
             case .fitness: UserActivityView()
             case .settings: SettingsView(tabBarHeight: tabBarHeight)
             }
--- ProLight/View/NewMapView/mapView.swift
+++ ProLight/View/NewMapView/mapView.swift
@@ -9,10 +9,169 @@ import SwiftUI
 import MapKit
 
 struct mapView: View {
-    
+    // MARK: Map Properties
+    @State private var cameraPosition: MapCameraPosition = .region(.myRegion)
+    @State private var mapSelection: MKMapItem?
+    @Namespace private var locationSpace
+    @State private var viewingRegion: MKCoordinateRegion?
+    // MARK: Search Bar
+    @State private var searchText: String = ""
+    @State private var showSearch: Bool = false
+    @State private var searchResults: [MKMapItem] = []
+    // MARK: Map Selection Detail Properties
+    @State private var showDetails: Bool = false
+    @State private var lookAroundScene: MKLookAroundScene?
     
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        NavigationStack {
+            Map(position: $cameraPosition, selection: $mapSelection, scope: locationSpace) {
+                /// Map Annotations
+                Annotation("Apple Park", coordinate: .myLocation) {
+                    ZStack {
+                        Image(systemName: "applelogo")
+                    }
+                }
+                
+                /// Simply Display Annotations as Marker, as we seen before
+                ForEach(searchResults, id: \.self) { mapItem in
+                    let placemark = mapItem.placemark
+                    Marker(placemark.name ?? "Place", coordinate: placemark.coordinate)
+                }
+                
+                /// To Show User Current Location
+                UserAnnotation()
+            }
+            .onMapCameraChange({ ctx in
+                viewingRegion = ctx.region
+            })
+            .overlay(alignment: .bottomTrailing) {
+                VStack(spacing: 15) {
+                    MapCompass(scope: locationSpace)
+                    MapPitchToggle(scope: locationSpace)
+                    MapUserLocationButton(scope: locationSpace)
+                }
+                .buttonBorderShape(.circle)
+                .padding()
+            }
+            .mapScope(locationSpace)
+            .navigationTitle("Map")
+            .navigationBarTitleDisplayMode(.inline)
+            /// Search Bar
+            .searchable(text: $searchText, isPresented: $showSearch)
+            /// Showing Trasnlucent ToolBar
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbarBackground(.ultraThinMaterial, for: .navigationBar)
+            .sheet(isPresented: $showDetails, content: {
+                MapDetails()
+                    .presentationDetents([.height(300)])
+                    .presentationBackgroundInteraction(.enabled(upThrough: .height(300)))
+                    .presentationCornerRadius(25)
+                    .interactiveDismissDisabled(true)
+            })
+        }
+        .onSubmit(of: .search) {
+            Task {
+                guard !searchText.isEmpty else { return }
+                
+                await searchPlaces()
+            }
+        }
+        .onChange(of: showSearch, initial: false) {
+            if !showSearch {
+                /// Clearing Search Results
+                searchResults.removeAll(keepingCapacity: false)
+                showDetails = false
+                /// Zooming out to the user Region when Search Cancelled
+                withAnimation(.snappy) {
+                    cameraPosition = .region(.myRegion)
+                }
+            }
+        }
+        .onChange(of: mapSelection) { oldValue, newValue in
+            /// Displaying Details about the Selected Place
+            showDetails = newValue != nil
+            /// Fetching Look Around Preview, Whenever Selection Changes
+            fetchLookAroundPreview()
+        }
+    }
+    
+    // Map Details View
+    @ViewBuilder
+    func MapDetails() -> some View {
+        VStack(spacing: 15) {
+            ZStack {
+                /// New Look Around API
+                if lookAroundScene == nil {
+                    // New Empty View API
+                    ContentUnavailableView("No Preview Available", systemImage: "eye.slash")
+                } else {
+                    LookAroundPreview(scene: $lookAroundScene)
+                }
+            }
+            .frame(height: 200)
+            .clipShape(.rect(cornerRadius: 15))
+            /// Close Button
+            .overlay(alignment: .topTrailing) {
+                Button(action:{
+                    /// Closing View
+                    showDetails = false
+                    withAnimation(.snappy) {
+                        mapSelection = nil
+                    }
+                }, label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.title)
+                        .foregroundStyle(.black)
+                        .background(.white, in: .circle)
+                })
+                .padding(10)
+            }
+            
+            /// Direction's Button
+            Button("Get Directions") {
+                
+            }
+            .foregroundStyle(.white)
+            .frame(maxWidth: .infinity)
+            .padding(.vertical)
+            .background(.blue.gradient, in: .rect(cornerRadius: 15))
+        }
+        .padding(15)
+    }
+    
+    // MARK: Search Places
+    func searchPlaces() async {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchText
+        request.region = viewingRegion ?? .myRegion
+        
+        let results = try? await MKLocalSearch(request: request).start()
+        searchResults = results?.mapItems ?? []
+    }
+    
+    // MARK: Fetching Location Preview
+    func fetchLookAroundPreview() {
+        if let mapSelection {
+            /// Clearing Old One
+            lookAroundScene = nil
+            Task {
+                let request = MKLookAroundSceneRequest(mapItem: mapSelection)
+                lookAroundScene = try? await request.scene
+            }
+        }
+    }
+    
+    // MARK: Fetching Route
+    func fetchRoute() {
+        if let mapSelection {
+            let request = MKDirections.Request()
+            request.source = .init(placemark: .init(coordinate: .myLocation))
+            request.destination = mapSelection
+            
+            Task {
+                
+            }
+        }
     }
 }
 
